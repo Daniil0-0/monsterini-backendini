4327e2d9915f33c0cff7aad45fbfcf1919acbaf7
mj <mohamed-jan.jalloh@student.kdg.be>
2025-05-10 12:50:35 +0200

    backend connection made

--- src/main/java/be/kdg/hackathonsetup/domain/Questionnaire.java
+++ src/main/java/be/kdg/hackathonsetup/domain/Questionnaire.java
@@ -1,5 +1,6 @@
 package be.kdg.hackathonsetup.domain;
 
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 import jakarta.persistence.*;
 import lombok.Data;
 
@@ -20,6 +21,7 @@ public class Questionnaire {
 
     @OneToOne
     @JoinColumn(name = "user_id", referencedColumnName = "id")
+    @JsonManagedReference
     private MonsteriniUser user;
 
     public String enhancePrompt(String userPrompt) {
